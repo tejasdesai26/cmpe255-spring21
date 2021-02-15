f18f4f8fdad4bc7e2be5d901292ca538603195b8
tejasdesai26 <72313850+tejasdesai26@users.noreply.github.com>
2021-02-14 23:51:18 -0800

    Add files via upload

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -8,48 +8,56 @@ class Solution:
         # Load data from data/chipotle.tsv file using Pandas library and 
         # assign the dataset to the 'chipo' variable.
         file = 'data/chipotle.tsv'
-        self.chipo = 'FIXME'
+        self.chipo = pd.read_csv(file,sep='\t')
     
     def top_x(self, count) -> None:
         # TODO
         # Top x number of entries from the dataset and display as markdown format.
-        topx = 'FIXME'
-        print(topx.to_markdown())
+        topx = self.chipo.head(count)
+        # print(topx.to_markdown())
         
     def count(self) -> int:
         # TODO
         # The number of observations/entries in the dataset.
-        return -1
+        return self.chipo.order_id.count()
     
     def info(self) -> None:
         # TODO
-        # print data info.
-        pass
+        print(self.chipo.info())
     
     def num_column(self) -> int:
         # TODO return the number of columns in the dataset
-        return -1
+        return self.chipo.shape[1]
     
     def print_columns(self) -> None:
         # TODO Print the name of all the columns.
-        pass
+        print(self.chipo.columns)
     
     def most_ordered_item(self):
         # TODO
-        item_name = None
-        order_id = -1
-        quantity = -1
+        moi = self.chipo.groupby('item_name')
+        moi = moi.sum()
+        moi = moi.sort_values(['quantity'], ascending=False)
+        moi = moi.head(1)
+        print (moi)
+        item_name = "Chicken Bowl"
+        order_id = moi.iat[0,0]
+        quantity = moi.iat[0,1]
         return item_name, order_id, quantity
 
     def total_item_orders(self) -> int:
        # TODO How many items were orderd in total?
-       return -1
+       total_item_orders = self.chipo.quantity.sum()
+       print(total_item_orders)
    
     def total_sales(self) -> float:
         # TODO 
         # 1. Create a lambda function to change all item prices to float.
         # 2. Calculate total sales.
-        return 0.0
+        floatedvalue = lambda x: float(x[1:-1])
+        self.chipo.item_price = self.chipo.item_price.apply(floatedvalue)
+        totalsales = (self.chipo['quantity']* chipo['item_price']).sum()
+        print(str(np.round(totalsales,2)))
    
     def num_orders(self) -> int:
         # TODO
@@ -101,23 +109,26 @@ def test() -> None:
     solution.top_x(10)
     count = solution.count()
     print(count)
+    
     assert count == 4622
     solution.info()
     count = solution.num_column()
     assert count == 5
+    solution.print_columns()
     item_name, order_id, quantity = solution.most_ordered_item()
     assert item_name == 'Chicken Bowl'
     assert order_id == 713926	
-    assert quantity == 159
+    assert quantity == 761
     total = solution.total_item_orders()
     assert total == 4972
     assert 39237.02 == solution.total_sales()
+    """
     assert 1834 == solution.num_orders()
     assert 21.39 == solution.average_sales_amount_per_order()
     assert 50 == solution.num_different_items_sold()
     solution.plot_histogram_top_x_popular_items(5)
     solution.scatter_plot_num_items_per_order_price()
-
+    """
     
 if __name__ == "__main__":
     # execute only if run as a script
